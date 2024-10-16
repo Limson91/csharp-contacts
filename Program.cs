08f44ba6ccb2a0100baaafdfff9b58a13831af50
Limson91 <matteo.a.colautti@gmail.com>
2024-10-16 12:38:37 +0200

    add - search contacts

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@
 
     static void Main()
     {
+        System.Console.WriteLine();
         System.Console.WriteLine(" --- Select a number! ---");
         System.Console.WriteLine();
 
         while ( true ) {
             System.Console.WriteLine("1. Insert new contact");
             System.Console.WriteLine("2. View contacts");
+            System.Console.WriteLine("3. Search Contact");
             string choose = System.Console.ReadLine();
 
             switch ( choose ) {
@@ -23,6 +25,10 @@
 
                 case "2":
                 ViewContact();
+                break;
+
+                case "3":
+                SearchContact();
                 break;
             }
         }
@@ -36,15 +42,42 @@
             string name = System.Console.ReadLine();
             contacts[contactsCount++] = name;
             System.Console.WriteLine("Successfully added!");
+            System.Console.WriteLine();
         }
     }
 
-    static void ViewContact() {
+    static void ViewContact() 
+    {
         System.Console.WriteLine("Contacts list: ");
 
         for (int i = 0; i < contactsCount; i++)
         {
             System.Console.WriteLine(contacts[i]);
+        }
+
+        System.Console.WriteLine();
+    }
+
+    static void SearchContact()
+    {
+        System.Console.WriteLine("Search name: ");
+        string name = System.Console.ReadLine();
+        bool foundName = false;
+
+        for (int i = 0; i < contactsCount; i++)
+        {
+            if (contacts[i] == name) {
+                System.Console.WriteLine($"{name} has been found!");
+                System.Console.WriteLine();
+
+                foundName = true;
+                break;
+            }
+        }
+
+        if ( !foundName ) {
+            System.Console.WriteLine($"{name} not found!");
+            System.Console.WriteLine();
         }
     }
 }
