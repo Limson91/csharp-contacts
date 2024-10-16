59ed01787f5d0bb3b26047bbdf135dfa6e628177
Limson91 <matteo.a.colautti@gmail.com>
2024-10-16 12:18:34 +0200

    add - adding contact

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    // Array di contatti
+    static string[] contacts = new string[50];
+
+    // Contatore contatti presenti
+    static int contactsCount = 0;
+
+    static void Main()
+    {
+        System.Console.WriteLine(" --- Select a number! ---");
+        System.Console.WriteLine();
+
+        while ( true ) {
+            System.Console.WriteLine("1. Insert new contact");
+            string choose = System.Console.ReadLine();
+
+            switch ( choose ) {
+                case "1":
+                AddContact();
+                break;
+            }
+        }
+    }
+
+    static void AddContact()
+    {
+        if(contactsCount < contacts.Length) {
+            System.Console.WriteLine("Add contact name: ");
+
+            string name = System.Console.ReadLine();
+            contacts[contactsCount++] = name;
+            System.Console.WriteLine("Successfully added!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# C# - Contacts
+
+Il programma avrà le seguenti funzionalità:
+
+- Aggiunta di un contatto: l'utente può inserire il nome di un nuovo contatto e aggiungerlo alla lista dei contatti;
+- Visualizzazione di tutti i contatti: il programma mostra all'utente l'elenco completo dei contatti presenti nella lista;
+- Ricerca di un contatto per nome: l'utente può inserire il nome di un contatto e il programma cerca nella lista se esiste un contatto corrispondente;
+- Eliminazione di un contatto: l'utente può inserire il nome di un contatto e il programma lo elimina dalla lista se presente;
+- Uscita dal programma: l'utente può scegliere di uscire dal programma, interrompendo l'esecuzione.
