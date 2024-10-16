10c3b80770f0d315283f5a5f757ff89b44c0616c
Limson91 <matteo.a.colautti@gmail.com>
2024-10-16 12:30:06 +0200

    add - contacts list view

--- Program.cs
+++ Program.cs
@@ -13,11 +13,16 @@
 
         while ( true ) {
             System.Console.WriteLine("1. Insert new contact");
+            System.Console.WriteLine("2. View contacts");
             string choose = System.Console.ReadLine();
 
             switch ( choose ) {
                 case "1":
                 AddContact();
+                break;
+
+                case "2":
+                ViewContact();
                 break;
             }
         }
@@ -31,6 +36,15 @@
             string name = System.Console.ReadLine();
             contacts[contactsCount++] = name;
             System.Console.WriteLine("Successfully added!");
+        }
+    }
+
+    static void ViewContact() {
+        System.Console.WriteLine("Contacts list: ");
+
+        for (int i = 0; i < contactsCount; i++)
+        {
+            System.Console.WriteLine(contacts[i]);
         }
     }
 }
